91e088e3b256a15828abe0a99e61d9f49a644198
Daniel <daniel.lukic@student.tugraz.at>
2020-10-26 20:56:52 +0100

    1 dimension gaussian kernel as Initializer for kernel, continious cnn

--- odft_tools/layers.py
+++ odft_tools/layers.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
+import numpy as np
+from odft_tools.gaussian_kernel1D import GaussianKernel1D
+from odft_tools.init_ops_v2 import VarianceScaling
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.keras.engine.input_spec import InputSpec
+import six
+import functools
+from tensorflow.python.ops import nn_ops
 
 
 class IntegrateLayer(tf.keras.layers.Layer):
@@ -7,8 +15,8 @@ class IntegrateLayer(tf.keras.layers.Layer):
         self.h = h
 
     def call(self, inputs):
-        return self.h*tf.reduce_sum(
-            (inputs[:, :-1] + inputs[:, 1:])/2.,
+        return self.h * tf.reduce_sum(
+            (inputs[:, :-1] + inputs[:, 1:]) / 2.,
             axis=1, name='trapezoidal_integral_approx')
 
     def get_config(self):
@@ -17,17 +25,84 @@ class IntegrateLayer(tf.keras.layers.Layer):
         return config
 
 
-class ContinuousConv1D(tf.keras.layers.Layer):
+class ContinuousConv1D(tf.python.keras.layers.convolutional.Conv1D):
     """
     Arguments:
         filters: Integer, the dimensionality of the output space
             (i.e. the number of output filters in the convolution).
     """
 
-    def __init__(self, filters, **kwargs):
+    def __init__(self,
+                 mean=0,
+                 stddev=1,
+                 **kwargs):
         super().__init__(**kwargs)
-        # Add weights etc
 
-    def call(self, inputs):
-        # Actual implementation of the convolution goes here
-        pass
+        self.mean = mean
+        self.stddev = stddev
+        # self.kernel_initializer = VarianceScaling()
+        self.kernel_initializer = GaussianKernel1D(
+            mean=self.mean,
+            stddev=stddev
+        )
+
+    def build(self, input_shape):
+        input_shape = tensor_shape.TensorShape(input_shape)
+        input_channel = self._get_input_channel(input_shape)
+        if input_channel % self.groups != 0:
+            raise ValueError(
+                'The number of input channels must be evenly divisible by'
+                'thenumber of groups. Received groups={}, but the input has {}'
+                'channels (full input shape is {}).'.format(self.groups,
+                                                            input_channel,
+                                                            input_shape))
+
+        kernel_shape = self.kernel_size + (input_channel // self.groups,
+                                           self.filters)
+
+        self.kernel = self.add_weight(
+            name='kernel',
+            shape=kernel_shape,
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint,
+            trainable=True,
+            dtype=self.dtype)
+
+        if self.use_bias:
+            self.bias = self.add_weight(
+                name='bias',
+                shape=(self.filters,),
+                initializer=self.bias_initializer,
+                regularizer=self.bias_regularizer,
+                constraint=self.bias_constraint,
+                trainable=True,
+                dtype=self.dtype)
+        else:
+            self.bias = None
+        channel_axis = self._get_channel_axis()
+        self.input_spec = InputSpec(min_ndim=self.rank + 2,
+                                    axes={channel_axis: input_channel})
+
+        # Convert Keras formats to TF native formats.
+        if self.padding == 'causal':
+            tf_padding = 'VALID'  # Causal padding handled in `call`.
+        elif isinstance(self.padding, six.string_types):
+            tf_padding = self.padding.upper()
+        else:
+            tf_padding = self.padding
+        tf_dilations = list(self.dilation_rate)
+        tf_strides = list(self.strides)
+
+        tf_op_name = self.__class__.__name__
+        if tf_op_name == 'Conv1D':
+            tf_op_name = 'conv1d'  # Backwards compat.
+
+        self._convolution_op = functools.partial(
+            nn_ops.convolution_v2,
+            strides=tf_strides,
+            padding=tf_padding,
+            dilations=tf_dilations,
+            data_format=self._tf_data_format,
+            name=tf_op_name)
+        self.built = True
--- odft_tools/utils.py
+++ odft_tools/utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import tensorflow as tf
+
+from tensorflow.python.framework import dtypes
 
 def integrate(fun, h, method='trapezoidal'):
     if method == 'simple':
@@ -49,3 +52,56 @@ def second_derivative_matrix(G, h, method='three_point'):
         raise NotImplemented()
     mat /= h**2    
     return mat
+
+def gen_gaussian_kernel1D(shape, mean, stddev, dtype=dtypes.float32):
+    """ Returns a tensor object cotnaining gaussian kernels
+    Args:
+      shape: Shape of the tensor.
+      mean: mean of gaussian
+      stddev: stddev of gaussian
+      dtype: Optional dtype of the tensor. Only floating point types are
+         supported.
+    """ 
+
+    kernel_size = shape[0]
+    input_size = shape[1]
+    filter_size = shape[2]
+
+    gaus_kerne_count =  input_size * filter_size
+
+    truncate = kernel_size/2
+
+    width = int(truncate + 0.5)
+    # width = int(truncate * stddev + 0.5)
+    # width = truncate
+    support = np.arange(-width, width + 1)
+    
+    gauss_kernels = []
+
+
+    center = int(len(support)/2)
+    left_cut = center - int(kernel_size/2)
+    right_cut = center + int(kernel_size/2)
+
+    for i in range(gaus_kerne_count):
+        gauss_kernel = np.exp(-((support - mean) ** 2)/(2*stddev ** 2))
+        gauss_kernel = gauss_kernel / gauss_kernel.sum()
+
+        if (kernel_size % 2) != 0:
+            gauss_kernel = gauss_kernel[left_cut + 1:right_cut + 2]
+        else:
+            gauss_kernel = gauss_kernel[left_cut + 1:right_cut + 1]
+        gauss_kernels.append(gauss_kernel)
+
+    gauss_kernels = np.reshape(
+      gauss_kernels, (
+        filter_size,
+        input_size,
+        kernel_size
+        ) 
+      ).T
+
+    gauss_kernels = tf.convert_to_tensor(
+        value=gauss_kernels,
+        dtype=dtype)
+    return gauss_kernels
